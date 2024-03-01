e8b5f14de7e1ec7cbed02889f47b38ccb0b9dcfa
Ahmet Berke Tekin <ahmetberketekin@gmail.com>
2024-03-01 20:14:59 +0300

    try downloading pdf

--- controllers/engineController.js
+++ controllers/engineController.js
@@ -1,4 +1,9 @@
 const puppeteer = require('puppeteer')
+const https = require('https')
+const http = require('http')
+const fs = require('fs')
+const downloadPath =
+  'C:\\Kodlamalar\\JavaScriptCodes\\WebScrapingProject\\public\\PDFFiles\\'
 
 exports.scholarSearch = async (req, res) => {
   console.log(req.body.keyword)
@@ -16,55 +21,150 @@ exports.scholarSearch = async (req, res) => {
   await submitBtn.evaluate((btn) => btn.click())
   await page.waitForNavigation()
 
-  const titles = []
-  const urlAdresses = []
-  const quatotions = []
-
-  const divs = await page.$$eval('.gs_r.gs_or.gs_scl', (divs) => {
-    const results = []
-
-    divs.forEach((div) => {
-      const hasPdfText = div.textContent.includes('PDF')
-
-      if (hasPdfText) {
-        const links = div.querySelectorAll('.gs_ri > h3 > a')
-        links.forEach((link) => {
-          results.push({
-            text: link.textContent.trim(),
-            href: link.getAttribute('href'),
+  async function scrapeData(page) {
+    const divs = await page.$$eval('.gs_r.gs_or.gs_scl', (divs) => {
+      const results = []
+      //div objelerine atıyoruz sonuçları
+      divs.forEach((div) => {
+        const hasPdfText = div.textContent.includes('PDF')
+        if (hasPdfText) {
+          const links = div.querySelectorAll('.gs_ri > h3 > a')
+          links.forEach((link) => {
+            results.push({
+              text: link.textContent.trim(),
+              href: link.getAttribute('href'),
+            })
           })
-        })
-        const citations = div.querySelectorAll('.gs_fl.gs_flb > a')
-        citations.forEach((citation)=>{
-          if(citation.textContent.includes('Alıntılanma sayısı')){
-            if (citation.textContent.trim()) {
-              results.push({
-                divCitation: citation.textContent.trim()
-              })
+          //başka div objeleri
+          const citations = div.querySelectorAll('.gs_fl.gs_flb > a')
+          citations.forEach((citation) => {
+            if (citation.textContent.includes('Alıntılanma sayısı')) {
+              if (citation.textContent.trim()) {
+                results.push({
+                  divCitation: citation.textContent.trim(),
+                })
+              }
             }
-          }
-        })
-      }
+          })
+          const pdfLinks = div.querySelectorAll('.gs_or_ggsm > a')
+          pdfLinks.forEach((link) => {
+            results.push({
+              pdfLink: link.getAttribute('href'),
+            })
+          })
+        }
+      })
+      return results
     })
 
-    return results
-  })
+    const titles = findTitles(divs)
+    const urls = findUrls(divs)
+    const citations = findCitations(divs)
+    const pdfLinks = findPDFLinks(divs)
 
-  divs.forEach(({ text }) => titles.push(text))
-  divs.forEach(({ href }) => urlAdresses.push(href))
-  divs.forEach(({ divCitation }) => quatotions.push(divCitation))
-  const filteredTitles = titles.filter(function (element) {
-    return element !== undefined;
-    });
-  const filteredUrlAdresses = urlAdresses.filter(function (element) {
-    return element !== undefined;
-    });
-  const filteredCitations = quatotions.filter(function (element) {
-    return element !== undefined;
-    });
-  console.log(filteredTitles)
-  console.log(filteredUrlAdresses)
-  console.log(filteredCitations)
- 
+    return { titles, urls, citations, pdfLinks }
+  }
+
+  // Kullanım
+  const { titles, urls, citations, pdfLinks } = await scrapeData(page)
+  extractCitationNumber(citations)
+  console.log(titles)
+  console.log(urls)
+  console.log(citations)
+  console.log(pdfLinks)
+
+  const promises = pdfLinks.map((url, i) => {
+    return downloadPDF(url, `example${i}.pdf`)
+      .then(() => ({ status: 'fulfilled' }))
+      .catch((error) => ({ status: 'rejected', reason: error }))
+  })
+  Promise.allSettled(promises).then((results) => {
+    results.forEach((result, i) => {
+      if (result.status === 'fulfilled') {
+        console.log(`example${i}.pdf dosyası başarıyla indirildi.`)
+      } else {
+        console.error(
+          `example${i}.pdf dosyası indirilemedi. Hata: ${result.reason}`
+        )
+      }
+    })
+  })
   res.redirect('/')
+}
+// Fonksiyon 1: Başlık Bulma
+function findTitles(divs) {
+  const titles = []
+  divs.forEach(({ text }) => {
+    if (text) {
+      titles.push(text)
+    }
+  })
+  return titles
+}
+// Fonksiyon 2: URL Adresi Bulma
+function findUrls(divs) {
+  const urls = []
+  divs.forEach(({ href }) => {
+    if (href) {
+      urls.push(href)
+    }
+  })
+  return urls
+}
+function findCitations(divs) {
+  const citations = []
+  divs.forEach(({ divCitation }) => {
+    if (divCitation) {
+      citations.push(divCitation)
+    }
+  })
+  return citations
+}
+function findPDFLinks(divs) {
+  const pdfLinks = []
+  divs.forEach(({ pdfLink }) => {
+    if (pdfLink) {
+      pdfLinks.push(pdfLink)
+    }
+  })
+  return pdfLinks
+}
+function extractCitationNumber(citations) {
+  for (let i = 0; i < citations.length; i++) {
+    citations[i] = citations[i].replace(/[^0-9]/g, '')
+  }
+}
+function downloadPDF(url, destination) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(destination)
+    if (url.includes('https://')) {
+      https
+        .get(url, (response) => {
+          response.pipe(file)
+          file.on('finish', () => {
+            file.close()
+            resolve()
+          })
+        })
+        .on('error', (err) => {
+          fs.unlink(destination, () => {
+            reject(err.message)
+          })
+        })
+    } else if (url.includes('http://')) {
+      http
+        .get(url, (response) => {
+          response.pipe(file)
+          file.on('finish', () => {
+            file.close()
+            resolve()
+          })
+        })
+        .on('error', (err) => {
+          fs.unlink(destination, () => {
+            reject(err.message)
+          })
+        })
+    }
+  })
 }
